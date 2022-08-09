bfcdabe1101eda2370b73fec208fd8c5b4bcef7b
Albert Casas <albertcasas3d@gmail.com>
2022-08-09 23:02:49 +0200

    iterations 1, 2, 3 and 4 completed

--- vue-basics/src/components/NavBarFooter.vue
+++ vue-basics/src/components/NavBarFooter.vue
@@ -4,18 +4,23 @@
     <img class="logo" src="https://i0.wp.com/eltallerdehector.com/wp-content/uploads/2022/06/b79b0-batman-logo-png-images-download-free.png?resize=700%2C700&ssl=1" alt="logo">
     <ul class="nav-menu">
         <li><a href="#">Home</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact Us</a></li>
+        <li><a href="#">BatCave</a></li>
+        <li><a href="#">BatMobile</a></li>
+        <li><a href="#">Contact Alfred</a></li>
     </ul>
   </nav>
+  <h1>{{2+2}}</h1>  
+  <h1>{{iAmBatman}}</h1>
+  <h1 v-if="robin">{{robin}}</h1>
+  <h1 v-for="villain in villains" :key="villain">{{villain.name}} - {{villain.place}} - {{villain.realName}}</h1>
+
   <footer>
     <img class="footer-logo" src="https://i0.wp.com/eltallerdehector.com/wp-content/uploads/2022/06/b79b0-batman-logo-png-images-download-free.png?resize=700%2C700&ssl=1" alt="logo">
     <ul class="footer-menu">
         <li><a href="#">Home</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact Us</a></li>
+        <li><a href="#">BatCave</a></li>
+        <li><a href="#">BatMobile</a></li>
+        <li><a href="#">Contact Alfred</a></li>
     </ul>
   </footer>
   </div>
@@ -23,7 +28,19 @@
 
 <script>
 export default {
+    data() {
+        return {
+            iAmBatman: "Batman",
 
+            robin: "Robin",
+
+            villains: [
+                {name: "Joker", place: "Gotham City", realName: "Jack Napier"},
+                {name: "Catwoman", place: "Gotham City", realName: "Selina Kyle"},
+                {name: "Riddler", place: "Gotham City", realName: "Edward E. Nigma"}
+            ]
+        }
+    }
 }
 </script>
 
@@ -33,6 +50,7 @@ export default {
         display: flex;
         justify-content: space-around;
         align-items: center;
+        background-color: rgb(61, 58, 58);
     }
 
     .logo {
@@ -51,7 +69,9 @@ export default {
 
     .nav-menu a {
         text-decoration: none;
-        font-weight: 700
+        font-weight: 700;
+        color: white;
+        
     }
 
     footer {
@@ -59,6 +79,7 @@ export default {
         display: flex;
         justify-content: space-around;
         align-items: center;
+        background-color: rgb(61, 58, 58);
     }
 
     .footer-menu {
@@ -77,6 +98,7 @@ export default {
 
     .footer-menu a {
         text-decoration: none;
-        font-weight: 700
+        font-weight: 700;
+        color: white;
     }
 </style>
