752f9e27d078debc634c8f58b5ea00ee423f6571
Albert Casas <albertcasas3d@gmail.com>
2022-08-09 18:18:17 +0200

    adding navbar and footer

---
+++ vue-basics/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBarFooter/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import NavBarFooter from './components/NavBarFooter.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    NavBarFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-basics/src/components/NavBarFooter.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <nav class="navbar">
+    <img class="logo" src="https://i0.wp.com/eltallerdehector.com/wp-content/uploads/2022/06/b79b0-batman-logo-png-images-download-free.png?resize=700%2C700&ssl=1" alt="logo">
+    <ul class="nav-menu">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact Us</a></li>
+    </ul>
+  </nav>
+  <footer>
+    <img class="footer-logo" src="https://i0.wp.com/eltallerdehector.com/wp-content/uploads/2022/06/b79b0-batman-logo-png-images-download-free.png?resize=700%2C700&ssl=1" alt="logo">
+    <ul class="footer-menu">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact Us</a></li>
+    </ul>
+  </footer>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+    .navbar {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+
+    .logo {
+        width: 7rem;
+    }
+
+    .nav-menu {
+        display: flex;
+        justify-content: space-around;
+    }
+
+    .nav-menu li {
+        padding: 0 1rem;
+        list-style-type: none;
+    }
+
+    .nav-menu a {
+        text-decoration: none;
+        font-weight: 700
+    }
+
+    footer {
+        height: 10rem;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+
+    .footer-menu {
+        display: flex;
+        justify-content: space-around;
+    }
+
+    .footer-logo {
+        width: 10rem;
+    }
+
+    .footer-menu li {
+        padding: 0 1rem;
+        list-style-type: none;
+    }
+
+    .footer-menu a {
+        text-decoration: none;
+        font-weight: 700
+    }
+</style>
